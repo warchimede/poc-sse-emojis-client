f21d794d0032ecce59482ab9c0e412a44688c158
William Archimede <william.archimede@gmail.com>
2021-03-22 21:20:36 +0100

    Initial Commit

---
+++ poc-sse-emojis-client/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  poc-sse-emojis-client
+//
+//  Created by William Archimède on 22/03/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ poc-sse-emojis-client/poc_sse_emojis_clientApp.swift
@@ -0,0 +1,17 @@
+//
+//  poc_sse_emojis_clientApp.swift
+//  poc-sse-emojis-client
+//
+//  Created by William Archimède on 22/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct poc_sse_emojis_clientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
